bc0b21e587c38199b8b76ff2457008d89c69010e
Charvi-code <83527936+Charvi-code@users.noreply.github.com>
2021-10-25 18:52:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 song1="";
 song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+scoreLeftWrist=0;
+scoreRightWrist=0;
 function preload()
 {
     song1= loadSound("harry_potter_theme.mp3");
@@ -11,10 +17,53 @@ function setup()
     canvas.center();
     video= createCapture(VIDEO);
     video.hide();
-    song1.play();
-    song2.play();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        scoreLeftWrist= results[0].pose.keypoints[9].score;
+        scoreRightWrist= results[0].pose.keypoints[10].score;
+        console.log("score right Wrist= "+scoreRightWrist);
+        console.log("score Left Wrist= "+scoreLeftWrist);
+        leftWristX= results[0].pose.leftWrist.x;
+        leftWristY= results[0].pose.leftWrist.y;
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
+        rightWristX= results[0].pose.rightWrist.x;  
+        rightWristY= results[0].pose.rightWrist.y;
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
+    }
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!");
 }
 function draw()
 {
     image(video, 0,0,500,350);
+    fill("#ff0000");
+    stroke("#ff0000");
+    if(scoreLeftWrist>0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+        song1.stop();
+        if(song2.isPlaying()==false)
+        {
+            song2.play();
+            document.getElementById("song").innerHTML="Song= Into The Unknown";
+        }
+    }
+    if(scoreRightWrist>0.2)
+    {
+        circle(rightWristX,rightWristY,20);
+        song2.stop();
+        if(song1.isPlaying()==false)
+        {
+            song1.play();
+            document.getElementById("song").innerHTML="Song= Harry Potter Theme Song";
+        }
+    }
 }
